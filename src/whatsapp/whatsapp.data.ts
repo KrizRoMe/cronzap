e3cc2897c9d61f2af00d03644322b2c04784f160
KrizRoMe <cmromero1231@gmail.com>
2025-01-07 10:44:00 -0500

    feat(whatsapp): :star: add whatsapp data

--- src/whatsapp/whatsapp.data.ts
+++ src/whatsapp/whatsapp.data.ts
@@ -1,8 +1,80 @@
 export const whatsappData = [
 	{
-		guest: 'Richard',
+		guest: 'Javier',
 		roomNumber: '1',
-		phoneNumber: '51957786975',
+		phoneNumber: '51970190686',
 		paymentDay: '07',
+	},
+	{
+		guest: 'Wilder',
+		roomNumber: '2',
+		phoneNumber: '51900526460',
+		paymentDay: '06',
+	},
+	{
+		guest: 'Jesus',
+		roomNumber: '4',
+		phoneNumber: '51942009634',
+		paymentDay: '02',
+	},
+	{
+		guest: 'Garay',
+		roomNumber: '5',
+		phoneNumber: '51900898799',
+		paymentDay: '14',
+	},
+	{
+		guest: 'Diana',
+		roomNumber: '6',
+		phoneNumber: '51974578318',
+		paymentDay: '21',
+	},
+	{
+		guest: 'Maicol',
+		roomNumber: '7',
+		phoneNumber: '51922718881',
+		paymentDay: '27',
+	},
+	{
+		guest: 'Luis',
+		roomNumber: '9',
+		phoneNumber: '51952458056',
+		paymentDay: '11',
+	},
+	{
+		guest: 'Joseph',
+		roomNumber: '10',
+		phoneNumber: '51991087254',
+		paymentDay: '16',
+	},
+	{
+		guest: 'Roberto',
+		roomNumber: '11',
+		phoneNumber: '51932583450',
+		paymentDay: '05',
+	},
+	{
+		guest: 'Gerardo',
+		roomNumber: '12',
+		phoneNumber: '51993079266',
+		paymentDay: '04',
+	},
+	{
+		guest: 'Efrain',
+		roomNumber: '13',
+		phoneNumber: '51951359186',
+		paymentDay: '17',
+	},
+	{
+		guest: 'Rorin',
+		roomNumber: '14',
+		phoneNumber: '51920266651',
+		paymentDay: '05',
+	},
+	{
+		guest: 'Katty',
+		roomNumber: '15',
+		phoneNumber: '51992007318',
+		paymentDay: '22',
 	},
 ];
